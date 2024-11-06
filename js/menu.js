532f8ee2dc79aa2eb455472ac82684030cda0162
carloscabani <cacabani@espol.edu.ec>
2024-11-06 08:49:37 -0500

    Avance 2, pequenias funcionalidades

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,4 @@
-(function(){
+(function(){   //manejo de apertura y cierre de menu en disp moviles
     const openButton = document.querySelector('.nav__menu');
     const menu = document.querySelector('.nav__link');
     const closeMenu = document.querySelector('.nav__close');
@@ -11,4 +11,20 @@
         menu.classList.remove('nav__link--show');
     });
 
+    // Seleccionar todos los enlaces de navegación que tengan href con un #
+    document.querySelectorAll('.nav__links[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function(event) {
+            event.preventDefault(); // Previene el comportamiento predeterminado del enlace
+
+            // Selecciona la sección objetivo
+            const targetSection = document.querySelector(this.getAttribute('href'));
+
+            // Desplazamiento suave hacia la sección con duración personalizada
+            window.scrollTo({
+                top: targetSection.offsetTop,
+                behavior: 'smooth'
+            });
+        });
+    });
 })();
+
