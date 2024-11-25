a6003b2b9594dc28b9126bcf10e4f4cd6e0035c4
carloscabani <cacabani@espol.edu.ec>
2024-11-25 09:45:11 -0500

    Mejoras en navegacion

--- js/menu.js
+++ js/menu.js
@@ -11,5 +11,23 @@
     closeMenu.addEventListener('click', () => {
         menu.classList.remove('nav__link--show');
     });
+
+    // Manejo de desplazamiento suave para enlaces con hash
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault(); // Evita el comportamiento predeterminado
+            const targetId = this.getAttribute('href').slice(1); // Obtiene el ID del hash
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                targetElement.scrollIntoView({ behavior: 'smooth' }); // Desplazamiento suave
+            }
+
+            // Cerrar el menú si está abierto (en caso de dispositivos móviles)
+            if (menu.classList.contains('nav__link--show')) {
+                menu.classList.remove('nav__link--show');
+            }
+        });
+    });
 })();
 
